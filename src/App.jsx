c437662c994676a804579ce2d952b965bdaef218
ihossen016 <ihossen016@gmail.com>
2023-07-07 12:22:05 +0600

    hero section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodaysHotProducts } from "./store/todaysHotProductSlice";
-import { fetchHotProducts } from "./store/hotProductSlice";
 import { fetchFurnitureProducts } from "./store/furnitureSlice";
 import { fetchSearchProducts } from "./store/searchSlice";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
     const dispatch = useDispatch();
     const todaysHotProduct = useSelector(state => state.todaysHot);
-    const hotProduct = useSelector(state => state.hot);
     const furniture = useSelector(state => state.furniture);
     const search = useSelector(state => state.search);
 
     useEffect(() => {
         dispatch(fetchTodaysHotProducts());
-        dispatch(fetchHotProducts());
         dispatch(fetchFurnitureProducts());
         dispatch(fetchSearchProducts());
     }, []);
@@ -23,6 +21,7 @@ function App() {
     return (
         <div className="w-11/12 mx-auto border-red-700">
             <Header />
+            <Hero />
         </div>
     );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHotProducts } from "../store/hotProductSlice";
+
+const Hero = () => {
+    const dispatch = useDispatch();
+    const hotProduct = useSelector(state => state.hot);
+
+    useEffect(() => {
+        dispatch(fetchHotProducts());
+    }, []);
+
+    return (
+        <div className="py-16 flex justify-between items-center">
+            <h2 className="text-8xl w-2/5">
+                We picked some{" "}
+                <span className="text-[#FFAE5D] font-bold">cool things</span>{" "}
+                for you!
+            </h2>
+            <div className="w-2/5">
+                <p className="py-3 border-b-2 border-dashed border-[#FFAE5D] text-2xl font-bold">
+                    hot deals for you
+                </p>
+                <div className="grid grid-cols-3 gap-4 pt-3">
+                    {!hotProduct.loading &&
+                        hotProduct.products.map(item => (
+                            <div className="grid grid-cols-1" key={item._id}>
+                                <img src={item.product_img} alt="product" />
+                                <h3 className="text-base">
+                                    {item.product_name}
+                                </h3>
+                                <div className="flex justify-between items-center text-base">
+                                    <p>₹{item.product_price}</p>
+                                    <p className="text-red-500">
+                                        -{item.product_discount}% off
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
